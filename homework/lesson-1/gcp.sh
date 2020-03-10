006e54eb89d2b68a54704a39d95f2b1dab57da9c
Gregory Paton <gregpaton08@gmail.com>
2020-03-09 23:26:44 -0400

    Updates google cloud platform script to create a budget instance (since that isn't being preempted)

--- homework/lesson-1/gcp.sh
+++ homework/lesson-1/gcp.sh
@@ -6,10 +6,15 @@
 #
 ##########
 
+set -e
+
 export IMAGE_FAMILY="pytorch-latest-gpu" # or "pytorch-latest-cpu" for non-GPU instances
 export ZONE="us-west1-b"
-export INSTANCE_NAME="my-fastai-instance"
-export INSTANCE_TYPE="n1-highmem-8" # budget: "n1-highmem-4"
+# export ZONE="us-west2-b"
+export INSTANCE_NAME="my-fastai-instance-budget"
+# export INSTANCE_TYPE="n1-highmem-8"
+# budget:
+export INSTANCE_TYPE="n1-highmem-4"
 
 function usage {
     echo "usage: $0 [connect|create]"
@@ -19,14 +24,16 @@ function usage {
 }
 
 function create_gcp_instance {
-    echo "creating google cloud platform image..."
-    # budget: 'type=nvidia-tesla-k80,count=1'
+    echo "creating google cloud platform instance..."
+    # --accelerator="type=nvidia-tesla-p100,count=1" \
+    # budget:
+    # --accelerator="type=nvidia-tesla-k80,count=1" \
     gcloud compute instances create $INSTANCE_NAME \
             --zone=$ZONE \
             --image-family=$IMAGE_FAMILY \
             --image-project=deeplearning-platform-release \
             --maintenance-policy=TERMINATE \
-            --accelerator="type=nvidia-tesla-p100,count=1" \
+            --accelerator="type=nvidia-tesla-k80,count=1" \
             --machine-type=$INSTANCE_TYPE \
             --boot-disk-size=200GB \
             --metadata="install-nvidia-driver=True" \
@@ -34,7 +41,7 @@ function create_gcp_instance {
 }
 
 function connect_to_gcp_instance {
-    echo "connecting to google cloud platform image..."
+    echo "connecting to google cloud platform instance..."
     gcloud compute ssh --zone=$ZONE jupyter@$INSTANCE_NAME -- -L 8080:localhost:8080
 }
 
